c3a4808d276b294ee96d64fcf6ee153e1de304f0
turzacse <turzacse@gmail.com>
2024-01-02 19:28:16 +0600

    navber setup on the project

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,10 +1,14 @@
 import { Outlet } from "react-router-dom";
+import Navber from "../Shared/Navber";
 
 
 const Main = () => {
     return (
         <div>
+            <Navber/>
+            <div className="mx-10">
             <Outlet/>
+            </div>
         </div>
     );
 };
